aa0fdfc368da22a4ff1fdfd61e1e58618f09f559
Jan Szotkowski <szotkowski@raynet.cz>
2024-09-18 22:38:17 +0200

    nastaveni pravidel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import * as React from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+const App = (): React.ReactElement => {
+    return (
+        <div className={'content'}>
+            <h1>Rsbuild with React</h1>
+            <p>Start building amazing things with Rsbuild.</p>
+        </div>
+    );
 };
 
 export default App;
