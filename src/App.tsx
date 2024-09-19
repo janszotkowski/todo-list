f401bda92e09f519f7c297c98e92d2921eb5c326
Jan Szotkowski <szotkowski@raynet.cz>
2024-09-19 21:21:06 +0200

    [#1] vycisteni kodu

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import './App.scss';
 
 const App = (): React.ReactElement => {
     return (
-        <div className={'content'}>
-            <h1>Rsbuild with React</h1>
-            <p>Start building amazing things with Rsbuild.</p>
-        </div>
+        <main>
+            ahoj
+        </main>
     );
 };
 
