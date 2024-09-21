c246ace9d195d97dbf525d9ec06b9e4b89a13e4e
Jan Szotkowski <szotkowski@raynet.cz>
2024-09-21 23:41:35 +0200

    [#11] podpora pro lokalizaci

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import { Button, NextUIProvider, useDisclosure } from '@nextui-org/react';
+import { TodoItemInsertView } from '@/views';
+import { useTranslation } from 'react-i18next';
+import './i18n';
 import './styles/tailwind.scss';
 import './App.scss';
-import { TodoItemInsertView } from '@/views';
 
 const App = (): React.ReactElement => {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const {t, i18n} = useTranslation();
+
+    if (!i18n.isInitialized) {
+        return <div>Loading...</div>;
+    }
+
+    const changeLanguage = (lng: string): void => {
+        i18n.changeLanguage(lng);
+        localStorage.setItem('language', lng);
+    };
+
     return (
         <NextUIProvider>
             <main className={'vMain dark text-foreground bg-background'}>
+                <h1>{t('welcome')}</h1>
                 <Button
                     color={'primary'}
                     onClick={onOpen}
@@ -19,6 +33,8 @@ const App = (): React.ReactElement => {
                     isOpen={isOpen}
                     onOpenChange={onOpenChange}
                 />
+                <button onClick={() => changeLanguage('en')}>English</button>
+                <button onClick={() => changeLanguage('cs')}>Čeština</button>
             </main>
         </NextUIProvider>
     );
