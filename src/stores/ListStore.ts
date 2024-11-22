38661d24e1a7f547e90383696c12d211b0461a08
Jan Szotkowski <szotkowski.honz@seznam.cz>
2024-11-22 20:57:42 +0100

    update ListStore abstract

--- src/stores/ListStore.ts
+++ src/stores/ListStore.ts
@@ -1,5 +1,7 @@
-import { makeObservable, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
 import { LoadingStore } from '@/stores/LoadingStore.ts';
+import { ApiService } from '@/api/ApiService.ts';
+import { LoadingState } from '@/dto/Common.ts';
 
 export abstract class ListStore<L> extends LoadingStore {
     public data: L[] = [];
@@ -7,8 +9,27 @@ export abstract class ListStore<L> extends LoadingStore {
     public constructor() {
         super();
 
-        makeObservable<ListStore<L>>(this, {
+        makeObservable<ListStore<L>, any>(this, {
             data: observable,
+            setData: action,
         });
+    }
+
+    protected abstract getService(): ApiService<L>;
+
+    private setData(data: L[]): void {
+        this.data = data;
+    }
+
+    public async loadData(): Promise<void> {
+        this.setState(LoadingState.LOADING);
+
+        try {
+            const data = await this.getService().fetchAll() as L[];
+            this.setData(data);
+            this.setState(LoadingState.LOADED);
+        } catch {
+            this.setState(LoadingState.FAILURE);
+        }
     }
 }
