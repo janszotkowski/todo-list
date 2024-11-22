75d9df100669252a4a239ead05aeaad37a19f4ce
Jan Szotkowski <szotkowski.honz@seznam.cz>
2024-11-22 20:58:20 +0100

    update TodoItemListStore

--- src/stores/TodoItemListStore.ts
+++ src/stores/TodoItemListStore.ts
@@ -1,6 +1,10 @@
 import { TodoItem } from '@/dto/TodoItem.ts';
 import { ListStore } from '@/stores/ListStore.ts';
+import { ApiService } from '@/api/ApiService.ts';
+import { todoService } from '@/api/TodoService.ts';
 
 export class TodoItemListStore extends ListStore<TodoItem> {
-
+    protected getService(): ApiService<TodoItem> {
+        return todoService;
+    }
 }
