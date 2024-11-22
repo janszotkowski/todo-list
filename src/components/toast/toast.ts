04728e24096d8ce2b44aa55af610d9a6bed37e97
Jan Szotkowski <szotkowski.honz@seznam.cz>
2024-11-22 22:00:28 +0100

    refactor toast

--- src/components/toast/toast.ts
+++ src/components/toast/toast.ts
@@ -1,29 +1,21 @@
-import { Bounce, Id, toast } from 'react-toastify';
+import { Bounce, Id, toast, ToastOptions } from 'react-toastify';
+
+const options: ToastOptions = {
+    position: 'bottom-center',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored',
+    transition: Bounce,
+} as const;
 
 export const success = (message: string): Id => {
-    return toast.success(message, {
-        position: 'bottom-center',
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'colored',
-        transition: Bounce,
-    });
+    return toast.success(message, options);
 };
 
 export const error = (message: string): Id => {
-    return toast.error(message, {
-        position: 'bottom-center',
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'colored',
-        transition: Bounce,
-    });
+    return toast.error(message, options);
 };
