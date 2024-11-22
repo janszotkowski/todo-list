3c7f0825026519b99a9411ec414953ef49f5ef6d
Jan Szotkowski <szotkowski.honz@seznam.cz>
2024-11-22 20:59:31 +0100

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { Button, NextUIProvider, useDisclosure } from '@nextui-org/react';
-import { TodoItemInsertView } from '@/views';
+import { NextUIProvider } from '@nextui-org/react';
+import { TodoItemListView } from '@/views';
 import { useTranslation } from 'react-i18next';
 import './i18n';
 import './styles/tailwind.scss';
 
 const App = (): React.ReactElement => {
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
     const {t, i18n} = useTranslation();
 
     if (!i18n.isInitialized) {
@@ -35,18 +34,10 @@ const App = (): React.ReactElement => {
         <NextUIProvider>
             <main className={'w-screen h-screen dark text-foreground bg-background'}>
                 <h1>{t('welcome')}</h1>
-                <Button
-                    color={'primary'}
-                    onClick={onOpen}
-                >
-                    Click Me
-                </Button>
-                <TodoItemInsertView
-                    isOpen={isOpen}
-                    onOpenChange={onOpenChange}
-                />
                 <button onClick={() => changeLanguage('en')}>English</button>
                 <button onClick={() => changeLanguage('cs')}>Čeština</button>
+
+                <TodoItemListView />
             </main>
         </NextUIProvider>
     );
